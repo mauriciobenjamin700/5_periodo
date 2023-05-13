6b5a61feccfcaa7a9bdf8ce78d54ba794b10b16d
mauriciobenjamin700 <mauriciobenjamin700@gmail.com>
2023-05-13 16:27:13 -0300

    Estrutura de Dados I -> Q 10
    Lista de ponteiros, aprendendo a usar aritimetica de ponteiros.

---
+++ ESTRUTRA_DADOS_I/CODIGOS/LISTA_1/11.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define tam = 5
+
+
+
+int main(void){
+    int array[tam];
+    int *ponteiro;
+
+    for (ponteiro=array;ponteiro<array+tam){
+
+    }
+
+
+
+
+
+    return 0;
+}
