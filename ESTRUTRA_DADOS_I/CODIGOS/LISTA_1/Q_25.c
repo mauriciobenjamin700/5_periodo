e6ac675d7daf3548165a4d97b00bf78c7b14e6c5
LazimR <51827035+LazimR@users.noreply.github.com>
2023-05-15 17:34:32 -0300

    Update Q_25.c

--- ESTRUTRA_DADOS_I/CODIGOS/LISTA_1/Q_25.c
+++ ESTRUTRA_DADOS_I/CODIGOS/LISTA_1/Q_25.c
@@ -10,16 +10,22 @@ struct notas calcula_notas(struct notas n);
 
 int main(void){
 
+    struct notas n;
+    n = calcula_notas(ler_notas(n));
+    printf("Nota 1: %f\nNota 2: %f\nMedia S: %f\nMedia P: %f\n",n.nota1,n.nota2,n.media_simples,n.media_ponderada);
     return 0;
 }
 
 struct notas ler_notas(struct notas n){
-    scanf("%f",n.nota1);
-    scanf("%f",n.nota2);
+    scanf("%f",&n.nota1);
+    scanf("%f",&n.nota2);
 
     return n;
 }
 
 struct notas calcula_notas(struct notas n){
-    //Imagine uma continhas
+    n.media_simples = (n.nota1+n.nota2)/2;
+    n.media_ponderada = (n.nota1+n.nota2*2)/3;
+
+    return n;
 }
