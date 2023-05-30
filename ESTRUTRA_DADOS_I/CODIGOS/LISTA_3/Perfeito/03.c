d6a75c0d8ba7f59da8e4a2d4a2a1d250df534633
LazimR <51827035+LazimR@users.noreply.github.com>
2023-05-30 09:02:37 -0300

    Tome

--- ESTRUTRA_DADOS_I/CODIGOS/LISTA_3/Perfeito/03.c
+++ ESTRUTRA_DADOS_I/CODIGOS/LISTA_3/Perfeito/03.c
@@ -1,5 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+
+void imprimi_matriz(int linhas, int colunas,int **ponteiro)
+{
+    int i,j;
+
+    for (i=0;i<linhas;i++)
+    {
+        for(j=0;j<colunas;j++)
+        {
+            printf("%d ", ponteiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+int main(void)
+{
+    FILE *arquivo;
+    int **matriz;
+
+    int linhas , colunas;
+    int i,j;
+
+    //arquivo = fopen("C:\\Users\\Notebook\\Documents\\GitHub\5_periodo\\ESTRUTRA_DADOS_I\\CODIGOS\\LISTA_3\\input.txt","r");
+    arquivo = fopen("input.txt", "r");
+
+    if ( arquivo == NULL)
+    {
+        printf("\nErro ao abrir o arquivo");
+        return 1;
+    }
+
+    fscanf(arquivo, "%d", &linhas);
+    fscanf(arquivo, "%d", &colunas);    
+
+    //gerando matriz
+    matriz = (int**) calloc(linhas,sizeof(int*));
+
+    for(i=0;i<linhas;i++)
+    {
+        matriz[i] = (int*) calloc(colunas, sizeof(int));
+    }
+
+
+    for (i=0;i<linhas;i++)
+    {
+        for(j=0;j<colunas;j++)
+        {
+            //printf("Escaniei a posicao [%d] [%d]", i,j);
+            fscanf(arquivo, "%d", &matriz[i][j]);
+            printf("conteudo posicao [%d][%d] == %d", i,j, &matriz[i][j]);
+        }
+
+    }
+
+    imprimi_matriz(linhas, colunas, matriz);
+
+    //liberando memoria
+    for(i=0;i<linhas;i++)
+    {  
+        free(matriz[i]);
+
+    }
+    free(matriz);
+
+
+    fclose(arquivo);
+
+    return 0;
+=======
+>>>>>>> Stashed changes
 #include <math.h>
 
 #define MAX_SIZE 1000
@@ -128,4 +203,8 @@ void salvar_matriz(FILE* arquivo, int altura, int largura, int** matriz)
         }
         fprintf(arquivo, "\n");
     }
+<<<<<<< Updated upstream
+=======
+>>>>>>> 2543042f9ef4f10f9024d5da307cb25672fd2750
+>>>>>>> Stashed changes
 }
